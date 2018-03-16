dfe58bdf230d41fac577e23352ade5c55bf0c190
timothee.janmot <timothee.janmot@gmail.com>
2018-03-16 21:17:07 +0100

    update

---
+++ Assets/Scripts/tpDude.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tpDude : MonoBehaviour {
+
+	public TextMesh qT;
+	public TextMesh hT;
+
+	// Use this for initialization
+	void Start () {
+		hT.gameObject.SetActive (false); 
+	}
+	
+	// Update is called once per frame
+		void OnTriggerStay(Collider other) {
+			if (other.tag == "Player") {
+			
+	if (mD.isEnterPressed) {}
+			
+			}
+		}
+}
