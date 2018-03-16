339f7e4c7a0a1d1ce0ec5294df996fc8a03969c8
timothee.janmot <timothee.janmot@gmail.com>
2018-03-16 22:33:36 +0100

    add enemis

--- Assets/Scripts/tpDude.cs
+++ Assets/Scripts/tpDude.cs
@@ -1,23 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class tpDude : MonoBehaviour {
 
 	public TextMesh qT;
 	public TextMesh hT;
 
-	// Use this for initialization
-	void Start () {
-		hT.gameObject.SetActive (false); 
+    private bool isEnnemy;
+    // Use this for initialization
+    void Start () {
+        isEnnemy = false;
 	}
-	
-	// Update is called once per frame
-		void OnTriggerStay(Collider other) {
+
+    void Update()
+    {
+        if (!isEnnemy)
+        {
+            hT.gameObject.SetActive(false);
+            qT.gameObject.SetActive(true);
+        } else
+        {
+            hT.gameObject.SetActive(true);
+            qT.gameObject.SetActive(false);
+        }
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isEnnemy = false;
+        }
+    }
+    // Update is called once per frame
+    void OnTriggerStay(Collider other) {
 			if (other.tag == "Player") {
-			
-	if (mD.isEnterPressed) {}
-			
+                moveDude mD = other.GetComponent<moveDude>();
+                isEnnemy = true;
+                if (mD.isEnterPressed) {
+                    SceneManager.LoadScene("indoorTest", LoadSceneMode.Single);
+                }			
 			}
 		}
 }
