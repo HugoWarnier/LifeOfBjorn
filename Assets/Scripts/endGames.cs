fc8e3ba0a03b38c6df891db748db8dbee3ed4ee7
timothee.janmot <timothee.janmot@epita.fr>
2018-03-19 19:53:03 +0100

    add end & update speed peaolpe

---
+++ Assets/Scripts/endGames.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endGames : MonoBehaviour
+{
+
+
+    public TextMesh qT;
+    public TextMesh hT;
+    public GameObject boss;
+    private bool isEnnemy;
+
+    public GameObject dancing1;
+    public GameObject dancing2;
+    public GameObject dancing3;
+    public GameObject dancing4;
+    // Use this for initialization
+    void Start()
+    {
+        isEnnemy = false;
+    }
+
+    void Update()
+    {
+        if (boss != null)
+        {
+            hT.gameObject.SetActive(false);
+            qT.gameObject.SetActive(true);
+            dancing1.SetActive(false);
+            dancing2.SetActive(false);
+            dancing3.SetActive(false);
+            dancing4.SetActive(false);
+        }
+        else
+        {
+            dancing1.SetActive(true);
+            dancing2.SetActive(true);
+            dancing3.SetActive(true);
+            dancing4.SetActive(true);
+            hT.gameObject.SetActive(true);
+            qT.gameObject.SetActive(false);
+        }
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isEnnemy = false;
+        }
+    }
+    // Update is called once per frame
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            moveDude mD = other.GetComponent<moveDude>();
+            isEnnemy = true;
+        }
+    }
+}
--- Assets/Scripts/noreturnSpawn.cs
+++ Assets/Scripts/noreturnSpawn.cs
@@ -8,26 +8,57 @@ public class noreturnSpawn : MonoBehaviour
     public GameObject c1;
     public GameObject c2;
     public GameObject c3;
+    public GameObject player;
+    private moveDude mD;
+
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    public Transform camTransform;
+
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+
+    private bool declanched = false;
+    Vector3 originalPos;
+
     // Use this for initialization
     void Start()
     {
+
+        mD = player.GetComponent<moveDude>();
         c1.SetActive(false);
         c3.SetActive(false);
         c2.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-
+        if (shakeDuration > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            mD.enabled = true;
+            shakeDuration = 0f;
+            originalPos = camTransform.localPosition;
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
 
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !declanched)
         {
-            Debug.Log("JEPASEE");
+            declanched = true;
+            mD.enabled = false;
+            shakeDuration = 3;
             c1.SetActive(true);
             c3.SetActive(true);
             c2.SetActive(true);
